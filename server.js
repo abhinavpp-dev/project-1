f02c68965ee656e5ca967fadfdd4241173acd10d
abhinav <abhinavpp4326@gmail.com>
2025-02-10 14:10:18 +0530

    port number changed

--- server.js
+++ server.js
@@ -39,8 +39,8 @@ app.use('/',adminroutes);
 
 
 //start server
-const PORT=process.env.PORT||3001
+const PORT=process.env.PORT
 app.listen(PORT,()=>{
-  console.log('server started');
+  console.log('server started',PORT);
 })
 
