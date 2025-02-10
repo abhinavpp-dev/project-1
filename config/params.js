3d6ca0e2abf7106a13b68d1de4ccd62b02f3a6d8
abhinav <abhinavpp4326@gmail.com>
2025-02-10 11:43:38 +0530

    updated config

--- config/params.js
+++ config/params.js
@@ -1,4 +1,15 @@
 // config/params.js
+let folder = 'menu';
+let allowedFormats = ['jpg', 'jpeg', 'png', 'webp'];
+
+try {
+  const params = require('../config/params');
+  folder = params.folder || folder;
+  allowedFormats = params.allowedFormats || allowedFormats;
+} catch (err) {
+  console.warn('params.js not found or invalid, using default values.');
+}
+
 module.exports = {
   folder: 'menu',
   allowedFormats: ['jpg', 'jpeg', 'png', 'webp']
